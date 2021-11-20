b84502d4ef2d09aecd07188db79f395d5e3a29e7
Firo <mail@firo.it>
2021-11-20 10:18:18 +0100

    Added comment for tokens

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const { App } = require('@slack/bolt');
 This sample slack application uses SocketMode
 For the companion getting started setup guide,
 see: https://slack.dev/bolt-js/tutorial/getting-started
+and for token: https://slack.dev/bolt-js/tutorial/getting-started#tokens-and-installing-apps
 */
 
 // Initializes your app with your bot token and app token
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
+  token: process.env.SLACK_BOT_TOKEN, // SLACK_BOT_TOKEN=xoxb-<your-bot-token>
   socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN
+  appToken: process.env.SLACK_APP_TOKEN // SLACK_APP_TOKEN=xapp-<your-app-token>
 });
 
 (async () => {
