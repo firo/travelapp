8c3cfbb55763d11c44f83bd30edf4790424d0cae
Firo <mail@firo.it>
2021-11-20 10:38:32 +0100

    Added app mention and action for travel request

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const { App } = require('@slack/bolt');
+require("dotenv").config();
+const { App } = require("@slack/bolt");
+var new_req_block = require('./blocks/new_req_block.json');
 
 /*
 This sample slack application uses SocketMode
@@ -12,6 +14,23 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN, // SLACK_BOT_TOKEN=xoxb-<your-bot-token>
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN // SLACK_APP_TOKEN=xapp-<your-app-token>
+});
+
+// Listens app_mention event and answer with block message
+app.event('app_mention', async ({body, event, say}) => {
+  console.log('body: ', event);
+  await say(new_req_block);
+});
+
+// Send back to block the Acknowledge while filling the form
+app.action('static_select-action', ({ ack }) => ack());
+app.action('datepicker-action', ({ ack }) => ack());
+
+// Listen action event and answe whith messages
+app.action('req-submit', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> your travel request is submitted successfully!`);
 });
 
 (async () => {
