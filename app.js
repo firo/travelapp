3289caacab57f7f5d328cb4eaa4900f7377d8cae
Firo <mail@firo.it>
2021-11-20 01:21:02 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const { App } = require('@slack/bolt');
+
+// Initialize Bolt app, using the default HTTPReceiver
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  customRoutes: [
+    {
+      path: '/health-check',
+      method: ['GET'],
+      handler: (req, res) => {
+        res.writeHead(200);
+        res.end('Health check information displayed here!');
+      },
+    },
+  ],
+});
+
+(async () => {
+  await app.start();
+  console.log('⚡️ Bolt app started');
+})();
