e20cd21ad8ce2b0737465fecd11f098bd6facd0e
Firo <mail@firo.it>
2021-11-20 11:14:59 +0100

    Added reaction added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { App } = require("@slack/bolt");
 var new_req_block = require('./blocks/new_req_block.json');
+let message; // Store message
 
 /*
 This sample slack application uses SocketMode
@@ -32,6 +33,41 @@ app.action('req-submit', async ({ body, ack, say }) => {
   await ack();
   await say(`<@${body.user.id}> your travel request is submitted successfully!`);
 });
+
+// Listens to reactio  "white_check_mark" and say "Your travel is approved!"
+app.event('reaction_added', ({body, event, say}) => {
+  if (event.reaction == 'white_check_mark' ){
+    fetchMessage(event.item.channel, event.item.ts, function(res) {
+      console.log('response: '+ res);
+      say(`<@${event.user}> Your travel is approved!`);
+    });
+  }
+
+});
+
+// Fetch conversation history using the ID and a TS from the last example
+async function fetchMessage(id, ts, callback) {
+  try {
+    // Call the conversations.history method using the built-in WebClient
+    const result = await app.client.conversations.history({
+      // The token you used to initialize your app
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: id,
+      // In a more realistic app, you may store ts data in a db
+      latest: ts,
+      // Limit results
+      inclusive: true,
+      limit: 1
+    });
+
+    message = result.messages[0];
+    console.log('retrive result: ' + JSON.stringify(result));
+    return callback(message.blocks[1].elements[0].value);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
 
 (async () => {
   const port = 3000
